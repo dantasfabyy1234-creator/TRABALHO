57f58315631fe8f8653fd76c93128e40fa70c4db
LaraSantana27 <llarasantana27@gmail.com>
2026-07-03 15:51:00 -0300

    Classes
    discutimos algumas partes que estão faltando

--- StockFlow/ParteInicial.ts
+++ StockFlow/ParteInicial.ts
@@ -10,32 +10,53 @@
 // cpf
 // nome loja (se vender o produto em lojas)
 // quatidade produto
-// id produto
+// id produto           
 
 
 
 
 
 class Cliente{
-    nome:string
-    contato:number
-    cpf:number
+    private nome:string
+    private contato:number
+    private cpf:number
+    private tipoCliente:string//PF ou CNPJ 
+    private endereco: string
 
-    constructor(nome:string, contato:number, cpf:number){
+
+    constructor(nome:string, contato:number, cpf:number, tipoCliente:string, endereco:string){
         this.nome = nome
         this.contato = contato
         this.cpf = cpf
+        this.tipoCliente = tipoCliente
+        this.endereco = endereco
     }
 }
-class Produto{
+
+class Estoque{
     nomeProduto: string
     material: string
     idProduto: number
+    quantidade: number
+    preco: number
 
-    constructor(nomeProduto: string, material: string, idProduto: number){
+    constructor(nomeProduto: string, material: string, idProduto: number, quantidade: number, preco: number){
         this.nomeProduto = nomeProduto
         this.material = material
         this.idProduto = idProduto
+        this.quantidade = quantidade
+        this.preco = preco 
+
+    }
+     
+    venda(quantidade, idProduto): 
+}
+
+let vetor[]:Estoque 
+
+class Venda{
+    constructor(nome:string){
+        super(nome)
     }
 
 }
